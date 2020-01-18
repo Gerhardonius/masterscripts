8a6889d6715e41a625186e9e15bb2f7d677dbb4f
gerhard.ungersbaeck <gerhard.ungersbaeck@clip-login-1.cbe.vbc.ac.at>
2020-01-18 19:42:55 +0100

    bugfix singularityscripts

--- processing/eventgen/singularityscripts/SMsingularitywrapper.sh
+++ processing/eventgen/singularityscripts/SMsingularitywrapper.sh
@@ -12,7 +12,7 @@ ml --latest singularity
 SOURCEELMODELDIR=/mnt/hephy/pheno/gerhard/Madresults/singularity/v1/SM/SM_DYjets_dielec
 SOURCEMUMODELDIR=/mnt/hephy/pheno/gerhard/Madresults/singularity/v1/SM/SM_DYjets_dimuon
 #SOURCEELMODELDIR=/mnt/hephy/pheno/gerhard/Madresults/singularity/firsttry
-MODELNAME="SM"
+MODEL="SM"
 NRE=50000
 
 #
@@ -47,35 +47,43 @@ fi
 #
 
 # copy the compiled dir
-MODELDIR=${SOURCEMODELDIR}_${MODELNAME}_${MLLRANGE}_${RUNNUMBER}
+MODELNAME=${MODEL}_${MLLRANGE}_${RUNNUMBER}
+MODELDIR=${SOURCEMODELDIR}_${MODELNAME}
+echo "cp -r ${SOURCEMODELDIR} ${MODELDIR}"
 cp -r ${SOURCEMODELDIR} ${MODELDIR}
+echo "cp completed"
 
 #
 # write madevent commands and call singulartiy shell
 #
 
-touch tmpcmd
-echo "generate_events ${MODELNAME}_${CHANNEL}_${MLLRANGE}_${RUNNUMBER}"	>> tmpcmd
-#echo "launch ${MODELNAME}"			>> tmpcmd
-echo "set nevents ${NRE}" 			>> tmpcmd
+touch tmpcmd${MODELNAME}
+echo "launch"					>> tmpcmd${MODELNAME}
+echo "shower=Pythia8"				>> tmpcmd${MODELNAME}
+echo "detector=Delphes"				>> tmpcmd${MODELNAME}
+#echo "generate_events ${MODELNAME}"		>> tmpcmd${MODELNAME}
+echo "done"	 				>> tmpcmd${MODELNAME}
+echo "set run_tag ${MODELNAME}"			>> tmpcmd${MODELNAME}
+echo "set nevents ${NRE}" 			>> tmpcmd${MODELNAME}
 if [ "$MLLRANGE" = "lo" ]
 then
-	echo "set mmll 50" 				>> tmpcmd
-	echo "set mmllmax 500" 				>> tmpcmd
+	echo "set mmll 50" 				>> tmpcmd${MODELNAME}
+	echo "set mmllmax 500" 				>> tmpcmd${MODELNAME}
 elif [ "$MLLRANGE" = "hi" ]
 then
-	echo "set mmll 500" 				>> tmpcmd
-	echo "set mmllmax -1" 				>> tmpcmd
+	echo "set mmll 500" 				>> tmpcmd${MODELNAME}
+	echo "set mmllmax -1" 				>> tmpcmd${MODELNAME}
 else
 	echo "usage: sbatch --job-name=test --output=test.log --error=test.err SMsingularitywrapper.sh -c ee/mm -r lo/hi -n 3"
 	exit 0
 fi
-#echo "set gVl1x1 0.1" 				>> tmpcmd
-#echo "set gAl1x1 0.2" 				>> tmpcmd
-##echo "set gVl2x2 500" 			>> tmpcmd
-##echo "set gAl2x2 500" 			>> tmpcmd
-#echo "set MZp ${MASS}" 			>> tmpcmd
-#echo "set wzp ${WIDTH}" 			>> tmpcmd
+#echo "set gVl1x1 0.1" 				>> tmpcmd${MODELNAME}
+#echo "set gAl1x1 0.2" 				>> tmpcmd${MODELNAME}
+##echo "set gVl2x2 500" 			>> tmpcmd${MODELNAME}
+##echo "set gAl2x2 500" 			>> tmpcmd${MODELNAME}
+#echo "set MZp ${MASS}" 			>> tmpcmd${MODELNAME}
+#echo "set wzp ${WIDTH}" 			>> tmpcmd${MODELNAME}
+echo "done"	 				>> tmpcmd${MODELNAME}
 
-singularity exec /mnt/hephy/pheno/ubuntu1904sing34.simg ${MODELDIR}/bin/madevent tmpcmd
-rm tmpcmd
+singularity exec /mnt/hephy/pheno/ubuntu1904sing34.simg ${MODELDIR}/bin/madevent tmpcmd${MODELNAME}
+rm tmpcmd${MODELNAME}
